6b9bb59bcbe41be4415c25ebc5562d38121f0fa2
Holderness <andersonholderness@gmail.com>
2015-01-21 18:18:34 -0500

    renamed data files to be more explicit, started parsing data

--- lib/record_parser.rb
+++ lib/record_parser.rb
@@ -1,1 +1,27 @@
-record_parser.rb
+class ParseData
+  attr_reader :raw_data
+
+  require 'csv'
+
+  def initialize(file_path)
+    @raw_data = File.readlines(file_path)
+  end
+
+  def parse_data_into_array(raw_data)
+    csv_data = raw_data.map{ |line| line.gsub(/\n+/, "").gsub(/\s|\||\,/, ",") }
+    @parsed_csv_array = csv_data.map{|row| CSV.parse(row).flatten}
+  end
+
+end
+
+class TennisPlayerData < ParseData
+
+end
+
+class PoliticianData < ParseData
+
+end
+
+class HockeyPlayerData < ParseData
+
+end
